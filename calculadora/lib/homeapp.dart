ca412d1eb527b9472e4b26a9947435496ac8720d
Hugo Christian Senai <hugo.pessego@to.docente.senai.br>
2021-11-04 21:46:58 -0300

    Codigo final

--- calculadora/lib/homeapp.dart
+++ calculadora/lib/homeapp.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HomeApp extends StatefulWidget {
-  const HomeApp({ Key? key }) : super(key: key);
+  const HomeApp({Key? key}) : super(key: key);
 
   @override
   _HomeAppState createState() => _HomeAppState();
 }
 
 class _HomeAppState extends State<HomeApp> {
-
   String output = "0";
 
   String _out = "0";
@@ -16,34 +15,154 @@ class _HomeAppState extends State<HomeApp> {
   double num2 = 0.0;
   String operand = "";
 
-  buttonPressed(String btnVal){
+  buttonPressed(String btnVal) {
     print(btnVal);
 
-    if(btnVal=="C"){
+    if (btnVal == "C") {
       _out = "0";
       num1 = 0.0;
       num2 = 0.0;
       operand = "";
-    }else if(btnVal =="+" || btnVal =="-" || btnVal =="*" || btnVal =="/" ){
+    } else if (btnVal == "+" ||
+        btnVal == "-" ||
+        btnVal == "*" ||
+        btnVal == "/") {
       num1 = double.parse(output);
       operand = btnVal;
       _out = "0";
       output = output + btnVal;
-    }else if(btnVal == "."){
-      if(_out.contains(".")){
+    } else if (btnVal == ".") {
+      if (_out.contains(".")) {
         print("Já existe");
         return;
-      }else{
+      } else {
         _out = _out + btnVal;
       }
+    } else if (btnVal == "=") {
+      num2 = double.parse(output);
+      if (operand == "+") {
+        _out = (num2 + num1).toString();
+      }
+
+      if (operand == "-") {
+        _out = (num1 - num2).toString();
+      }
+
+      if (operand == "*") {
+        _out = (num2 * num1).toString();
+      }
+
+      if (operand == "/") {
+        _out = (num1 / num2).toString();
+      }
+
+      num1 = 0.0;
+      num2 = 0.0;
+    } else {
+      _out = _out + btnVal;
     }
+
+    //Metodo para atualização de tela
+    setState(() {
+      output = double.parse(_out).toStringAsFixed(2); //14,5 = 14,50
+    });
   }
 
+  Widget buildButton(String ButtonVal) {
+    return Expanded(
+        child: Container(
+      padding: EdgeInsets.all(0),
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.shade500,
+                offset: Offset(2.0, 2.0),
+                blurRadius: 8.0,
+                spreadRadius: 1.0),
+            BoxShadow(
+                color: Colors.white,
+                offset: Offset(-2.0, -2.0),
+                blurRadius: 8.0,
+                spreadRadius: 1.0),
+          ]),
+      child: MaterialButton(
+        padding: EdgeInsets.all(30.0),
+        child: Text(
+          ButtonVal,
+          style: TextStyle(fontSize: 22),
+        ),
+        onPressed: () => buttonPressed(ButtonVal),
+      ),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: Container(
+            child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 50.0),
+              child: Text(
+                output,
+                style: TextStyle(fontSize: 60.0),
+              ),
+            ),
+            Expanded(
+              child: Divider(),
+            ),
+            Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    buildButton("."),
+                    buildButton("C"),
+                    buildButton("<-"),
+                    buildButton("*")
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    buildButton("7"),
+                    buildButton("8"),
+                    buildButton("9"),
+                    buildButton("/")
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    buildButton("4"),
+                    buildButton("5"),
+                    buildButton("6"),
+                    buildButton("+")
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    buildButton("1"),
+                    buildButton("2"),
+                    buildButton("3"),
+                    buildButton("-")
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    buildButton("0"),
+                    buildButton("="),
+                  ],
+                ),
+              ],
+            )
+          ],
+        )),
+      ),
     );
   }
 }
